b1f3acdd7ebee5234529b45addf04c24cf7ea977
“mohamed <mohamed.elmorsy20@gmail.com>
2022-11-14 10:18:35 +0200

    ggg

--- lib/src/settings/settings_view.dart
+++ lib/src/settings/settings_view.dart
@@ -37,7 +37,7 @@ class SettingsView extends StatelessWidget {
             ),
             DropdownMenuItem(
               value: ThemeMode.light,
-              child: Text('Light Theme'),
+              child: Text('Light GGGG'),
             ),
             DropdownMenuItem(
               value: ThemeMode.dark,
